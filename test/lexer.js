9d46073d03bab4211e82ef8de55bdc4e14303190
Varun Ramesh <varunramesh@live.com>
2015-12-09 17:42:31 -0800

    Lexer tests

--- test/lexer.js
+++ test/lexer.js
@@ -5,6 +5,7 @@ var assert = require('chai').assert;
 var walk = require('fs-walk');
 
 var sources_dir = path.join(__dirname, 'galaxy/');
+var tokens_dir = path.join(__dirname, 'tokens/');
 
 // String.prototype.endsWith polyfill.
 if (!String.prototype.endsWith) {
@@ -25,6 +26,13 @@ describe('Galaxy Lexer', function() {
     if (filename.endsWith('.galaxy')) {
       it(path.join(rel_dir, filename), function() {
         var code = fs.readFileSync(path.join(basedir, filename), 'utf-8');
+        var tokens = lexer(code);
+
+        var expectedFile = path.join(tokens_dir, rel_dir, filename) + '.json';
+        if (fs.existsSync(expectedFile)) {
+          var expected = JSON.parse(fs.readFileSync(expectedFile, 'utf-8'));
+          assert.deepEqual(tokens, expected);
+        }
       });
     }
   });
